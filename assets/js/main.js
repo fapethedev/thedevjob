d6a3626f9043b4d1d1530e051e192c557e23f604
fapethedev <mpetrivlin@gmail.com>
2024-01-11 15:57:10 +0000

    UPDATE content on web portfolio ADD preloader, sticky-nav, footer and more content

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').delay(4000).slideUp('slow');
+    });
+
+  })(window.jQuery);
+
+
+//Sticky navbar
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        let navbar_height;
+        if (window.scrollY > 50) {
+            document.getElementById('navbar')
+                .classList
+                .add('fixed-top');
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+            document.getElementById('navbar').style.backgroundColor = '#F6F4F1FF';
+        } else {
+            document.getElementById('navbar')
+                .classList
+                .remove('fixed-top');
+            document.body.style.paddingTop = '0';
+            document.getElementById('navbar').style.backgroundColor = '#F6F4F1C1';
+        }
+    });
+});
