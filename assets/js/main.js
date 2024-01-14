efc7138d534bafff2020628bc0a32d26bffa0a37
fapethedev <mpetrivlin@gmail.com>
2024-01-14 19:16:12 +0000

    UPDATE web portfolio New styling for navbar update contact add portfolio page

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,11 @@ const carousel = new bootstrap.Carousel('#rssCarousel');
 //Enable Popovers on rss carousel item
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+//Enable Modal for portfolio project gallery
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
